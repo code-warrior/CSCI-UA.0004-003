96de757676bc03c50181b325b697dcb9d11ba312
Roy Vanegas <roy@thecodeeducators.com>
2019-01-08 14:23:18 -0500

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require(`gulp`);
+const del = require(`del`);
+const HTMLPreprocessor = require(`gulp-nunjucks-render`);
+const browserSync = require(`browser-sync`);
+const data = require(`gulp-data`);
+const reload = browserSync.reload;
+
+/**
+ * COMPILE HTML
+ */
+gulp.task(`compileHTML`, function () {
+    HTMLPreprocessor.nunjucks.configure({watch: false});
+
+    return gulp.src([
+        `./app/views/*.html/`,
+        `./app/views/**/*.html/`])
+        .pipe(data(function () {
+            return require(`./app/models/data.json`);
+        }))
+        .pipe(HTMLPreprocessor())
+        .pipe(gulp.dest(`./`));
+});
+
+/**
+ * SERVE
+ */
+gulp.task(`default`, [`compileHTML`],
+    function () {
+        browserSync({
+            notify: true,
+            port: 9000,
+            reloadDelay: 100,
+            server: {
+                baseDir: [`./`, `./app/`]
+            }
+        });
+
+        gulp.watch([
+            `./app/views/*.html`,
+            `./app/controllers/**/*.njk`,
+            `./app/controllers/*.njk`,
+            `./app/css/*.css`
+        ],
+        [`compileHTML`]).on(`change`, reload);
+    });
+
+/**
+ * CLEAN
+ */
+gulp.task(`clean`, function () {
+    var fs = require(`fs`),
+        i,
+        foldersToDelete = [``];
+
+    for (i = 0; i < foldersToDelete.length; i++) {
+        try {
+            fs.accessSync(foldersToDelete[i], fs.F_OK);
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory was found and will be deleted.\n`);
+            del(foldersToDelete[i]);
+        } catch (e) {
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory does NOT exist or is NOT accessible.\n`);
+        }
+    }
+
+    process.stdout.write(`\n`);
+});
